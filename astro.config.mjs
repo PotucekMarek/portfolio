cf98e8b3963f9850b72e4f93d1403ffceb286ffa
Marek <marek@Mareks-Mac-mini.local>
2026-05-27 20:00:43 +0200

    server deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://www.marekpotucek.com',
-  // Řádek s output: 'hybrid' jsme úplně smazali, Astro si samo doplní 'static'
-  adapter: cloudflare(),
+  // Všechny adaptéry jsou smazané, Astro teď ví, že jde o čistě statický web
 });
