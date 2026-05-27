d6cb0f38a42bedb1855d2a33aad5af31ec226556
Marek <marek@Mareks-Mac-mini.local>
2026-05-27 19:33:02 +0200

    fix: absolute cesta k npx

--- update_letaky.sh
+++ update_letaky.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# 1. Přesun do složky tvého projektu
+# Načtení běžných systémových cest
+export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+
+# 1. Přesun do složky projektu
 cd /Users/marek/Dev/marekpotucek/absent-atmosphere || exit
 
 # 2. Spuštění stahování
 python3 stahovac.py
 
-# 3. Nahrání hotového ZIPu přímo do Cloudflare R2
+# 3. Nahrání hotového ZIPu do Cloudflare R2 pomocí absolutní cesty k npx
 echo "Nahrávám do Cloudflare R2..."
-npx wrangler r2 object put letaky-sklad/letaky.zip --file=./letaky.zip
+/Users/marek/.nvm/versions/node/v24.15.0/bin/npx wrangler r2 object put letaky-sklad/letaky.zip --file=./letaky.zip
 
 echo "Hotovo! Letáky jsou na cloudu."
