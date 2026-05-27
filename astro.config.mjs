90148ef0a662f53d92ddb829594da03f4332ecf9
Marek <marek@Mareks-Mac-mini.local>
2026-05-27 20:04:48 +0200

    add static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://www.marekpotucek.com',
-  // Všechny adaptéry jsou smazané, Astro teď ví, že jde o čistě statický web
+  output: 'static'
 });
