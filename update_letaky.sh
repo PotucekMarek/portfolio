1c87e38cd4fa7501836a142cb4d75b4c6979e431
Marek <marek@Mareks-Mac-mini.local>
2026-05-28 20:05:31 +0200

    R2 path update

--- update_letaky.sh
+++ update_letaky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Načtení běžných systémových cest
-export PATH="/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+# Načtení systémových cest VČETNĚ tvé skryté cesty k Node.js (NVM)
+export PATH="/Users/marek/.nvm/versions/node/v24.15.0/bin:/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 
 # 1. Přesun do složky projektu
 cd /Users/marek/Dev/marekpotucek/absent-atmosphere || exit
@@ -9,8 +9,8 @@ cd /Users/marek/Dev/marekpotucek/absent-atmosphere || exit
 # 2. Spuštění stahování
 python3 stahovac.py
 
-# 3. Nahrání hotového ZIPu do Cloudflare R2 pomocí absolutní cesty k npx
+# 3. Nahrání hotového ZIPu do Cloudflare R2
 echo "Nahrávám do Cloudflare R2..."
-/Users/marek/.nvm/versions/node/v24.15.0/bin/npx wrangler r2 object put letaky-sklad/letaky.zip --file=./letaky.zip
+npx wrangler r2 object put letaky-sklad/letaky.zip --file=./letaky.zip
 
 echo "Hotovo! Letáky jsou na cloudu."
